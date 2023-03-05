7541d300623698dae9b6d692c59334ba2a5d87f4
VaquarShaikh <vaquarshaikh253@gmail.com>
2023-03-05 19:38:52 +0530

    Trying to check stash merge conflict .

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ function HomePage() {
       <div className='flex flex-col items-center justify-center'>
         <DocumentMagnifyingGlassIcon className='h-64 w-64 text-indigo-600/20' />
         <h1 className='text-3xl text-center mt-2 text-black font-bold mb-5'>
-          Hello world
+          Welcome to the Amazon Web Scraper .
         </h1>
         <h2 className='text-lg italic text-center text-black/50'>Just a random webscraper created by vaq .</h2>
         <h3 className='text-lg text-center italic text-black/50'>https://github.com/VaquarShaikh</h3>
