81feb19908f46109c97cbe558e3ecb3cbdda60b9
VaquarShaikh <vaquarshaikh253@gmail.com>
2023-03-05 19:55:30 +0530

    trying to mimic merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ function HomePage() {
         <DocumentMagnifyingGlassIcon className='h-64 w-64 text-indigo-600/20' />
         <h1 className='text-3xl text-center mt-2 text-black font-bold mb-5'>
           Welcome to the Amazon Web Scraper .
+          sdsdf
         </h1>
         <h2 className='text-lg italic text-center text-black/50'>Just a random webscraper created by vaq .</h2>
         <h3 className='text-lg text-center italic text-black/50'>https://github.com/VaquarShaikh</h3>
